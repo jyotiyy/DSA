de726702168ddd7bf758a07e7887d5a8470f9e71
jyotiyy <whyjyoti24@gmail.com>
2026-08-05 09:14:03 +0530

    05-07

--- Breadth-First-Search/Remove-Methods-From-Project.cpp
+++ Breadth-First-Search/Remove-Methods-From-Project.cpp
@@ -9,6 +9,10 @@ to remove all suspicious nodes or not suspicious.test(i) && inDegree(i) > 0
 We have used bitset here , cuz it stores only 1 bit for one entry unlike bool which would have taken 4 bytes :(( 
 Many insights on the way... ;)
 
+Talking about time Complexity O(V+E);
+Space complexity: O(n + m)
+
+----------------- BFS -----------------------------
 class Solution {
 public:
     static constexpr int MAXN = 100005;
@@ -54,5 +58,43 @@ public:
             return allnode;
         }
         return remain;
+    }
+};
+
+
+---------------------------- DFS --------------------------------
+class Solution {
+public:
+    void dfs(int node,unordered_map<int,vector<int>> &inv, vector<int> &vis){
+        vis[node] = 1;
+        for(auto &it: inv[node]){
+            if(!vis[it]){
+                dfs(it,inv,vis);
+            }
+        }
+    }
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& inv) {
+        unordered_map<int,vector<int>> invoke;
+        for(auto &it: inv){
+            invoke[it[0]].push_back(it[1]);
+        }
+        vector<int> vis(n,0);
+        dfs(k,invoke,vis);
+
+        vector<int> rem;
+        for(auto &it: inv){
+            if(!vis[it[0]] && vis[it[1]]){ // if a edge goes from non-suspicious to suspiciosus cant be removed sorry:)
+                for(int i = 0; i < n; i++){
+                    rem.push_back(i);
+                }
+                return rem;
+            }
+        }
+
+
+        for(int i = 0; i < n; i++){
+            if(!vis[i]) rem.push_back(i);
+        }
+        return rem;
     }
 };
