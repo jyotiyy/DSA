34dcee4ff35f8a55dc25b858aac73acca4cacecc
jyotiyy <whyjyoti24@gmail.com>
2026-09-03 05:44:35 +0530

    03-09-26

--- Heap/Top-K-frequent-Words.cpp
+++ Heap/Top-K-frequent-Words.cpp
@@ -1,3 +1,5 @@
+Just keep in mind compare the frequency , if frequency same compare the lexacographical order ;)
+
 class Solution {
 public:
     struct cmp{
@@ -27,6 +29,31 @@ public:
             pq.pop();
         }
         reverse(res.begin(),res.end());
+        return res;
+
+    }
+};
+
+Mera pehla wala bhi kaam kar gaya 
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string,int> freq;
+        for(string &w:words){
+            freq[w]++;
+        }
+        vector<pair<string,int>> fq(freq.begin(),freq.end());
+        sort(fq.begin(),fq.end(),[](auto &a,auto &b){
+            if(a.second == b.second){
+                return a.first < b.first;
+            }
+            return a.second > b.second;
+        });
+        vector<string> res;
+        for(int i = 0; i < k; i++){
+            res.push_back(fq[i].first);
+        }
+        // reverse(res.begin(),res.end());
         return res;
 
     }
